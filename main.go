61a954a3d08ad337300766e55ebcda4ce4804216
Mike Williams <mdub@dogbiscuit.org>
2017-05-18 09:57:26 +1000

    A starting point.

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "boom"
+	app.Usage = "make an explosive entrance"
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("boom! I say!")
+		return nil
+	}
+
+	app.Run(os.Args)
+}
