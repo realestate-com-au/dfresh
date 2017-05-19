674ea698f68b21e9151974fc81c2192e74157d5b
Mike Williams <mdub@dogbiscuit.org>
2017-05-19 10:48:35 +1000

    Pull in bits of docker/docker.

--- main.go
+++ main.go
@@ -4,17 +4,32 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/Sirupsen/logrus"
+	"github.com/docker/docker/cli/command"
+	"github.com/docker/docker/pkg/term"
 	"github.com/urfave/cli"
 )
 
 func main() {
 	app := cli.NewApp()
-	app.Name = "boom"
-	app.Usage = "make an explosive entrance"
-	app.Action = func(c *cli.Context) error {
-		fmt.Println("boom! I say!")
-		return nil
+	app.Name = "tagfush"
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "creds",
+			Aliases: []string{"c"},
+			Usage:   "show registry creds",
+			Action: func(c *cli.Context) error {
+				stdin, stdout, stderr := term.StdStreams()
+				logrus.SetOutput(stderr)
+
+				cli := command.NewDockerCli(stdin, stdout, stderr)
+				fmt.Println(cli)
+				return nil
+			},
+		},
 	}
 
 	app.Run(os.Args)
+
 }
