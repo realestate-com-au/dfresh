161d01f93bd99e591ac4a0fc13d0460534a36dea
Mike Williams <mdub@dogbiscuit.org>
2017-05-21 21:02:21 +1000

    Refactor.

--- app/app.go
+++ app/app.go
@@ -1,11 +1,16 @@
 package app
 
 import (
+	"fmt"
+
 	"github.com/Sirupsen/logrus"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/cli/command"
 	cliflags "github.com/docker/docker/cli/flags"
 	"github.com/docker/docker/pkg/term"
+
+	"github.com/docker/distribution/reference"
+	"github.com/docker/docker/registry"
 )
 
 var dockerCli *command.DockerCli
@@ -13,16 +18,34 @@ var dockerCli *command.DockerCli
 func Init() error {
 	stdin, stdout, stderr := term.StdStreams()
 	logrus.SetOutput(stderr)
-	cli := command.NewDockerCli(stdin, stdout, stderr)
+	dockerCli := command.NewDockerCli(stdin, stdout, stderr)
 	opts := cliflags.NewClientOptions()
-	err := cli.Initialize(opts)
+	err := dockerCli.Initialize(opts)
 	if err != nil {
 		return err
 	}
-	dockerCli = cli
 	return nil
 }
 
 func GetAuthFor(server string) (types.AuthConfig, error) {
 	return dockerCli.CredentialsStore("").Get(server)
+}
+
+func GetTags(s string) ([]string, error) {
+	var tags []string
+
+	ref, err := reference.ParseNormalizedNamed(s)
+	if err != nil {
+		return tags, err
+	}
+	fmt.Println(ref)
+
+	// Resolve the Repository name from fqn to RepositoryInfo
+	repoInfo, err := registry.ParseRepositoryInfo(ref)
+	if err != nil {
+		return tags, err
+	}
+	fmt.Println(repoInfo)
+
+	return []string{"hello"}, nil
 }
--- cmd/tags.go
+++ cmd/tags.go
@@ -3,8 +3,8 @@ package cmd
 import (
 	"fmt"
 
-	"github.com/docker/distribution/reference"
-	"github.com/docker/docker/registry"
+	"github.com/mdub/dfresh/app"
+
 	"github.com/spf13/cobra"
 )
 
@@ -14,20 +14,13 @@ func newTagsCmd() *cobra.Command {
 		Short: "Print all available tags for an image",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-
-			ref, err := reference.ParseNormalizedNamed(args[0])
+			tags, err := app.GetTags(args[0])
 			if err != nil {
 				return err
 			}
-			fmt.Println(ref)
-
-			// Resolve the Repository name from fqn to RepositoryInfo
-			repoInfo, err := registry.ParseRepositoryInfo(ref)
-			if err != nil {
-				return err
+			for _, tag := range tags {
+				fmt.Println(tag)
 			}
-			fmt.Println(repoInfo)
-
 			return nil
 		},
 	}
