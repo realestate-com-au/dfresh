912fdeae4ed2a358152da547da5e4da8cf94251d
Mike Williams <mdub@dogbiscuit.org>
2017-05-25 09:22:23 +1000

    Extract a "newRepository" function.

--- app/app.go
+++ app/app.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"errors"
+
 	"github.com/Sirupsen/logrus"
 	dist "github.com/docker/distribution"
 	"github.com/docker/distribution/reference"
@@ -14,6 +16,7 @@ import (
 
 var dockerCli *command.DockerCli
 
+// Initialise the app
 func Init(debug bool) error {
 	stdin, stdout, stderr := term.StdStreams()
 	logrus.SetOutput(stderr)
@@ -30,53 +33,50 @@ func Init(debug bool) error {
 	return nil
 }
 
+func newRepository(ctx context.Context, ref reference.Named) (dist.Repository, error) {
+
+	repoInfo, err := registry.ParseRepositoryInfo(ref)
+	if err != nil {
+		return nil, err
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"ref":  ref,
+		"repo": repoInfo,
+	}).Debug("repository found")
+
+	authConfig := command.ResolveAuthConfig(ctx, dockerCli, repoInfo.Index)
+	registryService := registry.NewService(registry.ServiceOptions{V2Only: true})
+	endpoints, err := registryService.LookupPullEndpoints(reference.Domain(repoInfo.Name))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, endpoint := range endpoints {
+		if endpoint.Version == registry.APIVersion1 {
+			continue
+		}
+		repository, confirmedV2, err := distribution.NewV2Repository(ctx, repoInfo, endpoint, nil, &authConfig, "pull")
+		if err == nil && confirmedV2 {
+			return repository, nil
+		}
+	}
+
+	return nil, errors.New("no V2 endpoint found")
+
+}
+
+// Get tags for a repository
 func GetTags(s string) ([]string, error) {
 	var tags []string
 	ref, err := reference.ParseNormalizedNamed(s)
 	if err != nil {
 		return tags, err
 	}
-	repoInfo, err := registry.ParseRepositoryInfo(ref)
-	if err != nil {
-		return tags, err
-	}
-	logrus.WithFields(logrus.Fields{
-		"ref":  ref,
-		"repo": repoInfo,
-	}).Debug("repository found")
-
 	ctx := context.Background()
-
-	authConfig := command.ResolveAuthConfig(ctx, dockerCli, repoInfo.Index)
-
-	registryService := registry.NewService(registry.ServiceOptions{V2Only: true})
-
-	// get endpoints
-	endpoints, err := registryService.LookupPullEndpoints(reference.Domain(repoInfo.Name))
+	repository, err := newRepository(ctx, ref)
 	if err != nil {
 		return tags, err
 	}
-
-	// retrieve repository
-	var (
-		confirmedV2 bool
-		repository  dist.Repository
-		lastError   error
-	)
-
-	for _, endpoint := range endpoints {
-		if endpoint.Version == registry.APIVersion1 {
-			continue
-		}
-
-		repository, confirmedV2, lastError = distribution.NewV2Repository(ctx, repoInfo, endpoint, nil, &authConfig, "pull")
-		if lastError == nil && confirmedV2 {
-			break
-		}
-	}
-	if lastError != nil {
-		return tags, lastError
-	}
-
 	return repository.Tags(ctx).All(ctx)
 }
