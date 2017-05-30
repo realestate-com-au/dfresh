a92920774843d09be8bf53166def4271658b2f34
David Yeung <david.yeung@rea-group.com>
2017-05-30 01:31:26 +0000

    Refactor update command to prepare for file input/output
    Extract update ref code so it can be called with different types of
    input and output. Add a unit test to give us a little more confidence.

--- cmd/update.go
+++ cmd/update.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"regexp"
 	"strings"
@@ -21,36 +22,43 @@ func newUpdateCmd(client rego.Client) *cobra.Command {
 		Short: "Update image references",
 		Args:  cobra.ExactArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			scanner := bufio.NewScanner(os.Stdin)
-			for scanner.Scan() {
-				var updateRef = func(s string) string {
-					parts := strings.Split(s, "@")
-					nameAndTag := parts[0]
-					oldDigest := parts[1]
-					ref, err := reference.ParseNormalizedNamed(nameAndTag)
-					if err != nil {
-						panic(err)
-					}
-					newRef, err := client.Resolve(ref)
-					if err != nil {
-						logrus.WithFields(logrus.Fields{
-							"reference": nameAndTag,
-						}).Error("cannot resolve")
-						return s
-					}
-					newDigest := newRef.Digest().String()
-					if newDigest != oldDigest {
-						logrus.WithFields(logrus.Fields{
-							"was": oldDigest,
-							"now": newDigest,
-						}).Debug("updated " + nameAndTag)
-					}
-					return reference.FamiliarString(newRef)
-				}
-				updated := refRegexp.ReplaceAllStringFunc(scanner.Text(), updateRef)
-				fmt.Println(updated)
-			}
-			return scanner.Err()
+			return updateRefsInStream(client, os.Stdin, os.Stdout)
 		},
 	}
+}
+
+func updateRefsInStream(client rego.Client, input io.Reader, output io.Writer) (err error) {
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		updated := refRegexp.ReplaceAllStringFunc(scanner.Text(), func(s string) string {
+			return getUpdatedRef(client, s)
+		})
+		fmt.Fprintln(output, updated)
+	}
+	return scanner.Err()
+}
+
+func getUpdatedRef(client rego.Client, s string) string {
+	parts := strings.Split(s, "@")
+	nameAndTag := parts[0]
+	oldDigest := parts[1]
+	ref, err := reference.ParseNormalizedNamed(nameAndTag)
+	if err != nil {
+		panic(err)
+	}
+	newRef, err := client.Resolve(ref)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"reference": nameAndTag,
+		}).Error("cannot resolve")
+		return s
+	}
+	newDigest := newRef.Digest().String()
+	if newDigest != oldDigest {
+		logrus.WithFields(logrus.Fields{
+			"was": oldDigest,
+			"now": newDigest,
+		}).Debug("updated " + nameAndTag)
+	}
+	return reference.FamiliarString(newRef)
 }
