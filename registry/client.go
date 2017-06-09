a7cadba482be807849e1ccfad57720ead2886bd9
David Yeung <david.yeung@rea-group.com>
2017-06-09 07:53:41 +0000

    Override CertsDir in main instead.

--- registry/client.go
+++ registry/client.go
@@ -35,8 +35,6 @@ func (c *defaultClient) Init(debug bool) error {
 	c.dockerCli = clicmd.NewDockerCli(os.Stdin, os.Stdout, os.Stderr)
 	opts := cliflags.NewClientOptions()
 	opts.Common.Debug = debug
-  // feeling awkward about overriding this global variable here. The default is not appropriate for a cli tool.
-  dreg.CertsDir = ""
 	return c.dockerCli.Initialize(opts)
 }
 
