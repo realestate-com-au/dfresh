d66f5e2fe5f6a6df65959a35cd02e178a5a1763c
Mike Williams <mdub@dogbiscuit.org>
2017-06-22 23:19:59 +1000

    Add "--quiet" flag.

--- cmd/update.go
+++ cmd/update.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"bytes"
+	"io"
 	"os"
 	"regexp"
 
@@ -13,16 +15,27 @@ import (
 var refRegexp = regexp.MustCompile(reference.NameRegexp.String() + "(?::" + reference.TagRegexp.String() + ")?@" + reference.DigestRegexp.String() + "\\b")
 
 func newUpdateCmd(client rego.Client) *cobra.Command {
-	return &cobra.Command{
+
+	var quiet bool
+
+	command := &cobra.Command{
 		Use:   "update [flags] [FILE...]",
 		Short: "Update image references",
 		Args:  cobra.MinimumNArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			updater := update.NewUpdater(client, os.Stdin)
+			reportDestination := io.Writer(os.Stdout)
+			if quiet || len(args) == 0 {
+				reportDestination = new(bytes.Buffer)
+			}
+			updater := update.NewUpdater(client, reportDestination)
 			if len(args) == 0 {
 				return updater.UpdateRefsInStream("-", os.Stdin, os.Stdout)
 			}
 			return updater.UpdateRefsInFiles(args)
 		},
 	}
+	command.Flags().BoolVarP(&quiet, "quiet", "q", false, "be silent")
+
+	return command
+
 }
