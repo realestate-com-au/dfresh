366f332e3f286f5c2b9e416925c2a362244ecad5
Mike Williams <mdub@dogbiscuit.org>
2017-06-23 09:05:21 +1000

    Remove redundant code.

--- cmd/update.go
+++ cmd/update.go
@@ -4,15 +4,11 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
-	"regexp"
 
-	"github.com/docker/distribution/reference"
 	rego "github.com/realestate-com-au/dfresh/registry"
 	"github.com/realestate-com-au/dfresh/update"
 	"github.com/spf13/cobra"
 )
-
-var refRegexp = regexp.MustCompile(reference.NameRegexp.String() + "(?::" + reference.TagRegexp.String() + ")?@" + reference.DigestRegexp.String() + "\\b")
 
 func newUpdateCmd(client rego.Client) *cobra.Command {
 
