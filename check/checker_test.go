dceb77fd6d8695f2c50c800fda0191439731467f
Danial Pearce <danial.pearce@rea-group.com>
2017-07-14 17:03:37 +1000

    Slight refactor of test for dry-ness

--- check/checker_test.go
+++ check/checker_test.go
@@ -25,10 +25,8 @@ func (c *stubClient) Resolve(name reference.Named) (reference.Canonical, error) 
 	return reference.WithDigest(name, c.digest)
 }
 
-func TestCheckWithoutSha(t *testing.T) {
+func CheckStreamTest(oldDigest string, newDigest string, t *testing.T) {
 
-	oldDigest := "sha256:?"
-	newDigest := "sha256:08868d719684cf9cafacbaa1786ad01111332b4c1e65abd67833db603d8dab7f"
 	input := "line1\nruby:2.3@" + oldDigest + "\nline 3\n"
 	expectedOutput := strings.Replace(input, oldDigest, newDigest, 1)
 	expectedReport := "-:2: ruby:2.3\n  old " + oldDigest + "\n  new " + newDigest + "\n"
@@ -63,38 +61,9 @@ func TestCheckWithoutSha(t *testing.T) {
 }
 
 func TestCheckStream(t *testing.T) {
+	CheckStreamTest("sha256:a5ebd3bc0bf3881258975f8afa1c6d24429dfd4d7dd53a299559a3e927b77fd7", "sha256:08868d719684cf9cafacbaa1786ad01111332b4c1e65abd67833db603d8dab7f", t)
+}
 
-	oldDigest := "sha256:a5ebd3bc0bf3881258975f8afa1c6d24429dfd4d7dd53a299559a3e927b77fd7"
-	newDigest := "sha256:08868d719684cf9cafacbaa1786ad01111332b4c1e65abd67833db603d8dab7f"
-	input := "line1\nruby:2.3@" + oldDigest + "\nline 3\n"
-	expectedOutput := strings.Replace(input, oldDigest, newDigest, 1)
-	expectedReport := "-:2: ruby:2.3\n  old " + oldDigest + "\n  new " + newDigest + "\n"
-	expectedUpdates := 1
-
-	client := &stubClient{digest: digest.Digest(newDigest)}
-	inputReader := strings.NewReader(input)
-	outputWriter := new(bytes.Buffer)
-	reportWriter := new(bytes.Buffer)
-	c := NewChecker(client, reportWriter)
-
-	err := c.CheckStream("-", inputReader, outputWriter)
-	if err != nil {
-		t.Error("Did not expect error, ", err)
-	}
-
-	output := outputWriter.String()
-	if output != expectedOutput {
-		t.Errorf("expected output %q, got %q", expectedOutput, output)
-	}
-
-	report := reportWriter.String()
-	if report != expectedReport {
-		t.Errorf("expected report %q, got %q", expectedReport, report)
-	}
-
-	updateCount := c.UpdateCount()
-	if updateCount != expectedUpdates {
-		t.Errorf("expected %q updates, got %q", expectedUpdates, updateCount)
-	}
-
+func TestCheckWithoutSha(t *testing.T) {
+	CheckStreamTest("sha256:?", "sha256:08868d719684cf9cafacbaa1786ad01111332b4c1e65abd67833db603d8dab7f", t)
 }
